b9072c81e9fb9decca96d0e291726cc2745a57e2
Guy Mulligan <guymulligan@outlook.com>
2023-08-01 11:57:59 +0100

    if else fixed, images started to add

--- tringquiz/src/App.jsx
+++ tringquiz/src/App.jsx
@@ -4,138 +4,171 @@ import './App.css';
 const questions = [
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl: '/public/919e7cc0-fad4-4f82-bce9-50a4c4a54717.jpeg'
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 
   {
     question: 'Which animal is this?',
-    answer: '1',
+    answer: 'a',
+    imageUrl:
   },
 ];
 
 function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  // useState for current question. Starts at 0.
   const [userAnswer, setUserAnswer] = useState('');
+  // useState for answer of questions. Initial state is empty string
   const [showResult, setShowResult] = useState(false);
+  // useState for showing results? Come back to this
+  const [score, setScore] = useState(0)
 
   function handleAnswerChange(event) {
     setUserAnswer(event.target.value);
   }
+  // Updates when user types/changes input field.
+  // event = user's interaction with input field.
+  // event.target.value = user's interaction with input field / DOM element on which the event was triggered (input) / current value of input field
+  // setUserAnswer(event.target.value) = changes value of userAnswer to the new value in the input field
 
   function handleNextQuestion() {
     if (currentQuestion === questions.length - 1) {
@@ -145,13 +178,20 @@ function App() {
       const correctAnswerTrimmed = questions[currentQuestion].answer.toLowerCase().trim();
 
       if (userAnswerTrimmed === correctAnswerTrimmed) {
+        setScore(score+1);
         setCurrentQuestion((prev) => prev + 1);
         setUserAnswer('');
       } else {
-        alert('Incorrect answer. Please try again.');
+        setCurrentQuestion((prev) => prev + 1);
+        setUserAnswer('');
       }
     }
   }
+  // if (currentQuestion ===..... = checks if current question is the last Q. If it is, show results. If not, progress to next Q.
+  // userAnswerTrimmed = user's answer to current question, coverted to lowercase, removed leading/trailing spaces (removes case sensitivity)
+  // if (userAnswerTrimmed ===..... = checks if real answer and input answer are equal. Increase score by one. 
+  // setCurrentQuestion((prev) => prev + a) = progress to next Q.
+  // setUserAnswer('') = returns userAnswer to an empty string, ready for next Q.
 
   function handleRestartQuiz() {
     setCurrentQuestion(0);
@@ -165,6 +205,7 @@ function App() {
         <>
           <h1>Question {currentQuestion + 1}</h1>
           <p>{questions[currentQuestion].question}</p>
+          <img src={questions.imageUrl} />
           <input
             type="text"
             value={userAnswer}
@@ -175,14 +216,9 @@ function App() {
         </>
       ) : (
         <>
-          <h1>Quiz Completed!</h1>
-          <p>Your Results:</p>
-          {questions.map((q, index) => (
-            <p key={index}>
-              {q.question}: {userAnswer.toLowerCase().trim() === q.answer.toLowerCase().trim() ? 'Correct' : 'Incorrect'}
-            </p>
-          ))}
-          <button onClick={handleRestartQuiz}>Restart Quiz</button>
+          <h1>Quiz complete!</h1>
+          <p>Your correctly spied {score}/10 specimens!</p>
+          <button onClick={handleRestartQuiz}>Play Again?</button>
         </>
       )}
     </div>
