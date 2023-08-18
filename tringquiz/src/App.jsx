0549cf70e877ae195bc687f5c050ebc5312eaa53
Guy Mulligan <guymulligan@outlook.com>
2023-08-18 17:00:49 +0100

    img centered for Qs

--- tringquiz/src/App.jsx
+++ tringquiz/src/App.jsx
@@ -286,7 +286,7 @@ function App() {
         <div className='questionsTitle'><h1>Question {currentQuestion + 1}</h1></div>
         <div className='questionsCurrentQuestion'><h2>{shuffledQuestions[currentQuestion].question}</h2></div>
         <div className='questionsHint'><p>Hint: I'm in <b>Gallery {shuffledQuestions[currentQuestion].hint}!</b></p></div>
-        <div className='questionsIMG'><img src={shuffledQuestions[currentQuestion].imageUrl} alt="Animal"/></div>
+        <div className='questionsIMGdiv'><img className='questionsIMG' src={shuffledQuestions[currentQuestion].imageUrl} alt="Animal"/></div>
         <div className='questionsInput'>
           <input
             type="text"
