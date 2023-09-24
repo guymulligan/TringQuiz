5053cd537d448523b3b507f916bf57c755693a15
Guy Mulligan <guymulligan@outlook.com>
2023-09-24 13:37:43 +0100

    React added, not needed but maybe needed for testing. Testing now working

--- tringquiz/src/App.test.js
+++ tringquiz/src/App.test.js
@@ -1,11 +1,13 @@
+import React from 'react'
 import {render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import App from './App.jsx'
+import ReactDOM from 'react-dom';
 
 test('loads and displays greeting', async () => {
   // ARRANGE
-  render(<App/>)
+  render(<App />)
 
   // ASSERT
-  expect(screen.getByRole('Spy')).toHaveTextContent('Spy')
+  expect(screen.getByText('Spy')).toHaveTextContent('Spy')
 })
