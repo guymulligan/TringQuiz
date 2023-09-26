77443e22ca339b0c5140a69910ba4c7cd8ba4134
Guy Mulligan <guymulligan@outlook.com>
2023-09-26 16:29:16 +0100

    removed unnecessary annotatiions from tests

--- tringquiz/src/App.test.js
+++ tringquiz/src/App.test.js
@@ -26,10 +26,8 @@ test('clicking landingPageButton changes the page', () => {
 test('clicking questionsButton takes us to the next question', () => {
   const { container } = render(<App />);
   
-  // Verify that you are initially on the landing page
   expect(screen.getByText('Spy')).toBeInTheDocument();
 
-  // Find and click the 'landingPageButton' on the landing page
   const landingPageButton = screen.getByText('Start!');
   fireEvent.click(landingPageButton);
 
@@ -44,10 +42,8 @@ test('clicking questionsButton takes us to the next question', () => {
 test('clicking questionsButton 5 times takes us to the end screen', () => {
   const { container } = render(<App />);
   
-  // Verify that you are initially on the landing page
   expect(screen.getByText('Spy')).toBeInTheDocument();
 
-  // Find and click the 'landingPageButton' on the landing page
   const landingPageButton = screen.getByText('Start!');
   fireEvent.click(landingPageButton);
 
@@ -64,24 +60,20 @@ test('clicking questionsButton 5 times takes us to the end screen', () => {
 test('endScreenButton returns us to the questions', () => {
   const { container } = render(<App />);
   
-  // Verify that you are initially on the landing page
   expect(screen.getByText('Spy')).toBeInTheDocument();
 
-  // Find and click the 'landingPageButton' on the landing page
   const landingPageButton = screen.getByText('Start!');
   fireEvent.click(landingPageButton);
 
-  // Find and click the 'questionsButton' 5 times on the landing page
   const questionsButton = screen.getByText('Next Question')
   for (let i=0; i < 5; i++) {
   fireEvent.click(questionsButton);
   }
 
-  // Find and click the 'questionsButton' 5 times on the landing page
   const endScreenButton = screen.getByText('Play Again?')
   fireEvent.click(endScreenButton);
 
-
-  // After clicking the button, check that the page content changes
+  // After clicking the button, check that the page content changes and takes us back to Q1
   expect(screen.getByText('Question 1')).toBeInTheDocument();
 });
+
